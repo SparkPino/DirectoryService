1c165303ef57088f962a9d29f4db61375fbe638c
Spark <vladbezuglij0@gmail.com>
2026-07-02 00:15:44 +0200

    Update 2.0

--- DirectoryService/src/DirectoryService.Application/Departments/Queries/GetDepartments/GetDepartmentsValidation.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/Queries/GetDepartments/GetDepartmentsValidation.cs
@@ -8,13 +8,16 @@ public class GetDepartmentsValidation : AbstractValidator<GetDepartmentsQuery>
 {
     public GetDepartmentsValidation()
     {
-        RuleFor(a => a.Pagination.Page).Must(a => a > 0)
-            .WithError(Error.Validation("page.zero", "Page должно быть не меньше 1", "Page"));
+        When(a => a.Pagination is not null, () =>
+        {
+            RuleFor(a => a.Pagination.Page).Must(a => a > 0)
+                .WithError(Error.Validation("page.zero", "Page должно быть не меньше 1", "Page"));
 
-        RuleFor(a => a.Pagination.PageSize).Must(a => a < 100)
-            .WithError(Error.Validation("pageSize.out.of.range", "PageSize должно быть не больше 100", "PageSize"));
-
+            RuleFor(a => a.Pagination.PageSize).Must(a => a is > 0 || a <= 100)
+                .WithError(Error.Validation("pageSize.out.of.range", "PageSize должно быть не больше 100", "PageSize"));
+        });
         RuleFor(a => a.Search).Must(a => a is null || a.Length < 150)
-            .WithError(Error.Validation("search.to.longe", "Search должно содержать не больше 150 символов", "Search"));
+            .WithError(Error.Validation("search.to.longe", "Search должно содержать не больше 150 символов",
+                "Search"));
     }
 }
