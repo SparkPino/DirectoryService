b7fd2f5cba57dec14a287fb63c36a1ab60b80608
Spark <vladbezuglij0@gmail.com>
2026-07-24 23:38:36 +0200

    Update 2.0

--- DirectoryService/src/DirectoryService.Application/Departments/Queries/GetDirectChildrenDepartment/GetDirectChildrenDepartmentValidator.cs
+++ DirectoryService/src/DirectoryService.Application/Departments/Queries/GetDirectChildrenDepartment/GetDirectChildrenDepartmentValidator.cs
@@ -8,7 +8,7 @@ public class GetDirectChildrenDepartmentValidator : AbstractValidator<GetDirectC
 {
     public GetDirectChildrenDepartmentValidator()
     {
-        RuleFor(a => a.ParentId).NotNull().NotEmpty()
-            .WithError(Error.Validation(null, message: "Не может быть пустым или null"));
+        RuleFor(a => a.ParentId).NotEmpty()
+            .WithError(Error.Validation(null, message: "Не может быть пустым"));
     }
 }
